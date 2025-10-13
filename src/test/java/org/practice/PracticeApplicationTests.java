651c1916884b52ac40a33e754dd0c454b4cf64ca
yu-heejin <jkgt0521@naver.com>
2025-10-13 20:13:49 +0900

    chore: 패키지명 및 파일명 변경

--- src/test/java/org/practice/PracticeApplicationTests.java
+++ src/test/java/org/practice/PracticeApplicationTests.java
@@ -1,10 +1,10 @@
-package org.yogiyo;
+package org.practice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class YogiyoApplicationTests {
+class PracticeApplicationTests {
 
     @Test
     void contextLoads() {
